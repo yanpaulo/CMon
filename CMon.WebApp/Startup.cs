48bd46836866755475c14c24bc1ec3b201570f41
yanpa <yanpa@YAN-NOTE>
2017-03-23 11:08:21 -0300

    Add project files.

---
+++ CMon.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CMon.WebApp.Startup))]
+namespace CMon.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
