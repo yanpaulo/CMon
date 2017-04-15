5944c1e5671b1a6564fd86b7743df92506c29c73
Yan Paulo <yanpaulo@hotmail.com>
2017-04-14 21:48:40 -0300

    Created main view and view model.

---
+++ CMon.IoTApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMon.IoTApp.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private double _power;
+        private double _consumptionKW;
+        private TimeSpan _time;
+        public int Voltage { get; set; } = 220;
+
+        public decimal Tax { get; set; } = 0.5m;
+
+        public double Power
+        {
+            get { return _power; }
+            set { _power = value; }
+        }
+        public TimeSpan Time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+        
+        public double ConsumptionKW
+        {
+            get { return _consumptionKW; }
+            set { _consumptionKW = value; }
+        }
+        
+        public decimal ConsumptionMoney => (decimal)ConsumptionKW * Tax;
+
+    }
+}
