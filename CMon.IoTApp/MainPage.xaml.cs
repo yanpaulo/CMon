ae766a74bacfefb32a5baad83a71e28317068768
Yan Paulo <yanpaulo@hotmail.com>
2017-04-18 00:09:28 -0300

    Kinda fixed the "graphic going away" glitch.

--- CMon.IoTApp/MainPage.xaml.cs
+++ CMon.IoTApp/MainPage.xaml.cs
@@ -36,6 +36,7 @@ namespace CMon.IoTApp
         private DispatcherTimer _timer;
         private DataWriter _dataWriter;
         private DataReader _dataReader;
+        private Random rng = new Random();
 
         public MainPage()
         {
@@ -45,15 +46,15 @@ namespace CMon.IoTApp
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            string aqs = SerialDevice.GetDeviceSelector();
-            var dis = await DeviceInformation.FindAllAsync(aqs);
-            var info = dis.First();
+            //string aqs = SerialDevice.GetDeviceSelector();
+            //var dis = await DeviceInformation.FindAllAsync(aqs);
+            //var info = dis.First();
 
-            _device = await SerialDevice.FromIdAsync(info.Id);
-            _device.BaudRate = 9600;
-            _device.ReadTimeout = TimeSpan.FromMilliseconds(200);
-            _dataReader = new DataReader(_device.InputStream);
-            _dataWriter = new DataWriter(_device.OutputStream);
+            //_device = await SerialDevice.FromIdAsync(info.Id);
+            //_device.BaudRate = 9600;
+            //_device.ReadTimeout = TimeSpan.FromMilliseconds(200);
+            //_dataReader = new DataReader(_device.InputStream);
+            //_dataWriter = new DataWriter(_device.OutputStream);
 
             _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
             _timer.Tick += Timer_Tick;
@@ -64,19 +65,31 @@ namespace CMon.IoTApp
 
         private async void Timer_Tick(object sender, object e)
         {
-            _dataWriter.WriteString("0");
-            await _dataWriter.StoreAsync().AsTask();
+            //_dataWriter.WriteString("0");
+            //await _dataWriter.StoreAsync().AsTask();
 
-            var bytes = await _dataReader.LoadAsync(1024).AsTask();
-            var json = _dataReader.ReadString(bytes);
-            Debug.Write(json);
-            var reading = JsonConvert.DeserializeObject<Reading>(json);
-            
-            _viewModel.Power = _viewModel.Voltage * reading.Current;
-            _viewModel.ConsumptionKW += _viewModel.Power / (3600 * 1000);
+            //var bytes = await _dataReader.LoadAsync(1024).AsTask();
+            //var json = _dataReader.ReadString(bytes);
+            //Debug.Write(json);
+            //var reading = JsonConvert.DeserializeObject<Reading>(json);
+
+            //_viewModel.Power = _viewModel.Voltage * reading.Current;
+            //_viewModel.ConsumptionKW += _viewModel.Power / (3600 * 1000);
+            _viewModel.Power = rng.Next(10, 15);
             _viewModel.Time = DateTime.Now - _startDate;
             _viewModel.ChartMinimumTime = _viewModel.Time - TimeSpan.FromSeconds(60);
-            _viewModel.ChartItems.Add(new MainChartViewModelItem { Power = _viewModel.Power, Time = DateTime.Now - _startDate });
+
+
+            _viewModel.ChartItems.Add(new MainChartViewModelItem { Power = _viewModel.Power, Time = _viewModel.Time });
+            if (_viewModel.ChartItems.First().Time < _viewModel.ChartMinimumTime)
+            {
+                _viewModel.ChartItems.RemoveAt(0);
+                _viewModel.ChartMinimumTime = _viewModel.ChartItems.First().Time;
+            }
+            else
+            {
+                
+            }
         }
     }
 }
