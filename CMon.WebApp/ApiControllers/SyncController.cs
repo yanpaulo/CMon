45829d4dcc634d4597f49b9c8ce7d054e695b5db
Yan Paulo <yanpaulo@hotmail.com>
2017-04-25 01:26:40 -0300

    Created SyncController.

---
+++ CMon.WebApp/ApiControllers/SyncController.cs
@@ -0,0 +1,39 @@
+﻿using CMon.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CMon.WebApp.ApiControllers
+{
+    public class SyncController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //[HttpPost]
+        //[Route("api/Sync")]
+        public IHttpActionResult PostReadings(List<Reading> readings)
+        {
+            var user = db.Users.First();
+            foreach (var r in readings)
+            {
+                r.Id = 0;
+                r.Date = DateTime.Now;
+                user.Readings.Add(r);
+            }
+            
+            db.SaveChanges();
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+    }
+}
